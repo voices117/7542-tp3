84448530a5dfc037c77bf17284c6c8459861dc32
axl <voices117@gmail.com>
2018-04-30 12:01:03 -0300

    addig doc

--- server_tag_index.cpp
+++ server_tag_index.cpp
@@ -16,6 +16,12 @@ void Server::TagIndex::swap(TagIndex& l, TagIndex& r) {
     std::swap(l.hashes, r.hashes);
 }
 
+/**
+ * @brief Gets the list of hashes associated to a given tag.
+ *
+ * @param tag Tag to query.
+ * @return Set of associated hashes.
+ */
 const std::set<std::string>& Server::TagIndex::get_hashes(
     const std::string& tag) const {
     try {
@@ -25,6 +31,12 @@ const std::set<std::string>& Server::TagIndex::get_hashes(
     }
 }
 
+/**
+ * @brief Adds a new tag into the index.
+ *
+ * @param tag Name of the new tag.
+ * @param hashes List of associated hashes.
+ */
 void Server::TagIndex::add(const std::string& tag,
                            const std::set<std::string>& hashes) {
     /* checks if the tag already exists */
